169d8d2cf79d13213136fbe9dbc9145aa880d758
DanTl <daniil9@hotmail.com>
2019-11-12 23:24:47 -0800

    Update Program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -14,6 +14,7 @@ namespace WebApplication1
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //code was added in GitHub
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
